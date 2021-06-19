7769306aa2b9a6b694674e62e8fe9d5705cb96cf
paras <paraskangra14@gmail.com>
2021-06-19 16:03:16 +0530

    made navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,245 @@ import styled from "styled-components";
 import React from "react";
 
 const Header = (props) => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Content>
+        <Logo>
+          <a href="/home">
+            <img src="/images/home-logo.svg" alt="logo" />
+          </a>
+        </Logo>
+        <Search>
+          <div>
+            <input type="text" placeholder="Search"></input>
+          </div>
+          <SearchIcon>
+            <img src="/images/search-icon.svg" alt="" />
+          </SearchIcon>
+        </Search>
+        <Nav>
+          <NavListWrap>
+            <NavList className="active">
+              <a>
+                <img src="/images/nav-home.svg" alt="" />
+                <span>Home</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-network.svg" alt="" />
+                <span>My Network</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-jobs.svg" alt="" />
+                <span>Jobs</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-messaging.svg" alt="" />
+                <span>Messaging</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-notifications.svg" alt="" />
+                <span>Notifications</span>
+              </a>
+            </NavList>
+
+            <User>
+              <a>
+                <img src="/images/user.svg" alt="" />
+                <div className="text">
+                  <span>Me</span>
+                  <img src="/images/down-icon.svg" alt="" />
+                </div>
+              </a>
+
+              <SignOut>
+                <a>Sign Out</a>
+              </SignOut>
+            </User>
+            <Work>
+              <a>
+                <img src="/images/nav-work.svg" alt="" />
+                <span>
+                  Work
+                  <img src="/images/down-icon.svg" alt="" />
+                </span>
+              </a>
+            </Work>
+          </NavListWrap>
+        </Nav>
+      </Content>
+    </Container>
+  );
 };
 
 export default Header;
+
+const Container = styled.div`
+  background-color: white;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  position: fixed;
+  left: 0;
+  padding: 0 24px;
+  top: 0;
+  width: 100vw;
+  z-index: 100;
+`;
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  min-height: 100%;
+  max-width: 1128px;
+`;
+
+const Logo = styled.span`
+  margin-right: 8px;
+  font-size: 0px;
+`;
+
+const Search = styled.div`
+  opacity: 1;
+  position: relative;
+  flex-grow: 1;
+  & > div {
+    max-width: 280px;
+    input {
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      color: rgba(0, 0, 0, 0.9);
+      border-radius: 2px;
+      width: 218px;
+      padding: 0 8px 0 40px;
+      line-height: 1.75;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+
+      border: 2px solid #dce6f1;
+      vertical-align: text-top;
+      /* border-color: */
+    }
+  }
+`;
+const SearchIcon = styled.div`
+  width: 40px;
+  position: absolute;
+  top: 10px;
+  left: 2px;
+  z-index: 1;
+  border-radius: 0 2px 2px 0;
+  margin: 0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Nav = styled.nav`
+  margin-left: auto;
+  display: block;
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+
+const NavListWrap = styled.ul`
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  list-style-type: none;
+  align-items: center;
+  .active {
+    border-bottom: 2px solid rgba(0, 0, 0, 0.9);
+  }
+`;
+
+const NavList = styled.li`
+  display: flex;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: transparent;
+    flex-direction: column;
+
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 1.5;
+    min-height: 52px;
+    min-width: 80px;
+    text-decoration: none;
+    position: relative;
+
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      align-items: center;
+    }
+
+    @media (max-width: 768px) {
+      min-width: 70px;
+    }
+  }
+
+  &:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
+`;
+
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
+`;
+
+const User = styled(NavList)`
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  .text {
+    display: flex;
+  }
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
+`;
+
+const Work = styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.08);
+`;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,94 @@ import styled from "styled-components";
 import React from "react";
 
 const Home = (props) => {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section>
+        <h5>
+          <a>Hiring in a hurry? - </a>
+        </h5>
+        <p>
+          Find Talented props in record time with Upwork and keep buisness
+          moving.
+        </p>
+      </Section>
+
+      <Layout>
+        <div className="left">left</div>
+        <div className="main">main</div>
+        <div className="right">right</div>
+      </Layout>
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+
+const Content = styled.div`
+  max-width: 1128px;
+  margin: 0 auto;
+`;
+const Section = styled.section`
+  min-height: 50px;
+  padding: 16px;
+  box-sizing: content-box;
+  text-align: center;
+  text-decoration: underline;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h5 {
+    color: #0a66c2;
+    font-size: 14px;
+    a {
+      font-weight: 700;
+    }
+  }
+  p {
+    font-size: 14px;
+    color: #434649;
+    font-weight: 600;
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  .left {
+    flex: 0.2;
+    text-align: center;
+    margin: 2px;
+
+    border: 1px red solid;
+  }
+  .main {
+    flex: 0.6;
+    text-align: center;
+    border: 1px red solid;
+    margin: 2px;
+
+    display: flex;
+    justify-content: center;
+  }
+  .right {
+    margin: 2px;
+
+    flex: 0.2;
+    border: 1px red solid;
+    text-align: center;
+  }
+  @media (max-width: 768px) {
+    flex-direction: column;
+    padding: 0 5px;
+  }
+`;
